ab1dc8c429417ea6d3e80dc59bedd335f9604c37
Kartik <kartikkc397@outlook.com>
2022-11-08 14:06:18 +0530

    added router

--- ctf-infox/src/App.js
+++ ctf-infox/src/App.js
@@ -1,24 +1,49 @@
+import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
 import logo from './logo.svg';
+import Navbar from "./Components/nav";
+import Home from "./Components/homepage";
+import Challenge from "./Components/challengesTemp";
+import Login from "./Components/login";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        {/* <Navbar /> */}
+         <Route exact path="/">
+            <div className="Home">
+              <Home page="home" />
+            </div>
+          </Route>
+        {/*  <Route exact path="/challenges">
+            <div className="Challenges">
+              <Challenge page="challenges" />
+            </div>
+          </Route>
+          <Route exact path="/login">
+            <div className="login">
+              <Login page="login" />
+            </div>
+          </Route> */}
+      </Router>
+      {/* <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div> */}
+    </>
   );
 }
 
