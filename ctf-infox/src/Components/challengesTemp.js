121c1d6ab339c42410188efc52f865ffa9b672e0
Dhruv Aeron <dhruv.aeron3@gmail.com>
2022-11-14 13:38:52 +0530

    modal added

--- ctf-infox/src/Components/challengesTemp.js
+++ ctf-infox/src/Components/challengesTemp.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 const Challenge = () => {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <div className="overflow-hidden flex flex-row">
       {/* left */}
@@ -64,7 +66,14 @@ const Challenge = () => {
                     d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
                   />
                 </svg>
-                <div className="text-xl text-slate-700">Hint 01.</div>
+                <button
+                  className="text-xl text-slate-700"
+                  onClick={() => {
+                    setModalOpen(true);
+                  }}
+                >
+                  Hint 01.
+                </button>
               </div>
               <div className="bg-slate-300 cursor-pointer hover:bg-gray-300 shadow-lg rounded-xl px-6 py-2 border-b-2 hover:border-gray-800 border-gray-400 flex space-x-2 items-center text-slate-700">
                 <svg
@@ -81,7 +90,14 @@ const Challenge = () => {
                     d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
                   />
                 </svg>
-                <div className="text-xl text-slate-700">Hint 02.</div>
+                <button
+                  className="text-xl text-slate-700"
+                  onClick={() => {
+                    setModalOpen(true);
+                  }}
+                >
+                  Hint 02.
+                </button>
               </div>
               <div className="bg-slate-300 cursor-pointer hover:bg-gray-300 shadow-lg rounded-xl px-6 py-2 border-b-2 hover:border-gray-800 border-gray-400 flex space-x-2 items-center text-slate-700">
                 <svg
@@ -98,7 +114,14 @@ const Challenge = () => {
                     d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
                   />
                 </svg>
-                <div className="text-xl text-slate-700">Hint 03.</div>
+                <button
+                  className="text-xl text-slate-700"
+                  onClick={() => {
+                    setModalOpen(true);
+                  }}
+                >
+                  Hint 03.
+                </button>
               </div>
             </div>
           </div>
@@ -138,6 +161,7 @@ const Challenge = () => {
           </div>
         </div>
       </div>
+      {modalOpen && <Modal setOpenModal={setModalOpen} />}
     </div>
   );
 }; 
